6ba587768fd74bcbb5b9366e14e0db116174f06f
cjk <vmflsxj0@naver.com>
2022-02-14 22:42:24 +0900

    깃 강의방 개설

---
+++ Udemy-Lecture/Udemy_ASPNETCORE_MVC_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Udemy_ASPNETCORE_MVC_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
